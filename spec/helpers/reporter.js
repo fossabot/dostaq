c7a7142f49cdd9d40ebb993e43bd6d1c65603159
Yusril Herlian Syah <yusrilhsyah@gmail.com>
2018-03-29 21:59:57 +0700

    Renew test schema

--- spec/helpers/reporter.js
+++ spec/helpers/reporter.js
@@ -3,8 +3,6 @@ const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 jasmine.getEnv().clearReporters();               // remove default reporter logs
 jasmine.getEnv().addReporter(new SpecReporter({  // add jasmine-spec-reporter
   spec: {
-    displayPending: true,
-    displayFailuredSpec: true,
-    displaySpecDuration: true 
+    displayPending: true
   }
 }));
