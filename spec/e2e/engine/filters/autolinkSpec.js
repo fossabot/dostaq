a5f6907cd7ed3f1c1dccb1b95040f7d5e7f747c2
Yusril Herlian Syah <yusrilhsyah@gmail.com>
2018-03-29 22:05:50 +0700

    New line after LICENSE

--- spec/e2e/engine/filters/autolinkSpec.js
+++ spec/e2e/engine/filters/autolinkSpec.js
@@ -21,6 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+
 const utils = require('../../../utils')
 
 describe('Test filter autolink', () => {
